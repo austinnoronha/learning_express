0796b062b1d2bfe0e89450eee5a1ca42682d4101
Austin Noronha <austin_noronha@hotmail.com>
2020-06-13 23:22:40 +0530

    4. Express Model - Get members JSON from a file

---
+++ Members.js
@@ -0,0 +1,47 @@
+
+//App Status Members Data
+const members = [{
+    "id": 1,
+    "first_name": "Moe",
+    "last_name": "Galway",
+    "email": "mgalway0@ycombinator.com",
+    "gender": "Male",
+    "status": "inavtive"
+  }, {
+    "id": 2,
+    "first_name": "Otto",
+    "last_name": "Calltone",
+    "email": "ocalltone1@sohu.com",
+    "gender": "Male",
+    "status": "inavtive"
+  }, {
+    "id": 3,
+    "first_name": "Clemmy",
+    "last_name": "Chesterman",
+    "email": "cchesterman2@sitemeter.com",
+    "gender": "Male",
+    "status": "active"
+  }, {
+    "id": 4,
+    "first_name": "Leeland",
+    "last_name": "Simek",
+    "email": "lsimek3@etsy.com",
+    "gender": "Male",
+    "status": "inavtive"
+  }, {
+    "id": 5,
+    "first_name": "Cassie",
+    "last_name": "Rosengart",
+    "email": "crosengart4@thetimes.co.uk",
+    "gender": "Male",
+    "status": "active"
+  }, {
+    "id": 6,
+    "first_name": "Ardelia",
+    "last_name": "McCumesky",
+    "email": "amccumesky5@netscape.com",
+    "gender": "Female",
+    "status": "active"
+  }];
+
+  module.exports = members;
