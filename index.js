48e7883966591f49c498b7746395272c4fd6829f
Austin Noronha <austin_noronha@hotmail.com>
2020-06-13 23:53:00 +0530

    Node Express API - Get member data from ID

--- index.js
+++ index.js
@@ -16,8 +16,19 @@ app.use(logger);
 app.use(express.static(path.join(__dirname, "public")));
 
 //App dynamic APIs
+//Get all members
 app.get("/api/members", (req, res) => {
   res.json(members);
+});
+
+//Get member data
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the ID of ${req.params.id}` });
+  }
 });
 
 app.listen(PORT, () => {
