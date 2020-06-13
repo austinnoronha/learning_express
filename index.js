2c5abfdbf4f96fdc49eb9f0a2ad525ee14add258
Austin Noronha <austin_noronha@hotmail.com>
2020-06-13 22:54:54 +0530

    2. Node Express - static public directory serving HTML files

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
  * @desc    Express Start File
  */
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world!</h1>')
-})
+//App set static folder - with middleware
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log("App runngin on ", PORT );
