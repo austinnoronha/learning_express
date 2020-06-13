af7f45e8b5bfe85ffe61741e92fddbf3f2c0b101
Austin Noronha <austin_noronha@hotmail.com>
2020-06-13 23:25:34 +0530

    Node Middleware - create a logger middleware

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const members = require('./Members');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//Middleware - logger
+const logger = (req, res, next) => {
+    console.log("[APP_LOGGER] hello!");
+
+    //Call next middlewarte function in the stack
+    next();    
+};
+
+app.use(logger);
+
 //App set static folder - with middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
